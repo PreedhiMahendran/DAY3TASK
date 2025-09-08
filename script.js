5a6176284edc9218282f66f52a7fd6e8899d2692
preedhimahendran <preedhi0011.a@gmail.com>
2025-09-08 13:34:33 +0400

    5th Question

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ for(let i=2;i<=10;i+=2){
 for(let i=100;i>=1;i-=10){
     console.log(i);
 }
+
+//Fifth question
+var arr;
+function voters(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]>=18) {
+      console.log("eligible to vote");
+    } else if (arr[i]===0) {
+      console.log("Invalid");
+    }else if(arr[i]<18){
+      console.log("not eligibe");
+    }
+}
+}voters([34,27,0,10,19,8,56,95,0,2,18])
